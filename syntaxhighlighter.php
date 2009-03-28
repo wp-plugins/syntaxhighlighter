a438d5a3b680b8b99cb0c973f3c0a1b55ab5d05f
Viper007Bond <Viper007Bond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-28 12:38:04 +0000

    Tons of settings page improvements and some tweaks to parameters and defaults
    git-svn-id: https://plugins.svn.wordpress.org/syntaxhighlighter/branches/development@105865 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -73,7 +73,7 @@ class SyntaxHighlighter {
 		// Register hooks
 		add_action( 'admin_menu',                   array(&$this, 'register_settings_page') );
 		add_action( 'admin_post_syntaxhighlighter', array(&$this, 'save_settings') );
-		add_action( 'admin_head',                   array(&$this, 'output_javascript_shortcodes') );
+		add_action( 'admin_head',                   array(&$this, 'admin_head') );
 		add_filter( 'the_content',                  array(&$this, 'parse_shortcodes'),          9 );
 		add_action( 'wp_footer',                    array(&$this, 'maybe_output_scripts'),      15 );
 		add_filter( 'mce_external_plugins',         array(&$this, 'add_tinymce_plugin') );
@@ -142,7 +142,6 @@ class SyntaxHighlighter {
 			'firstline'  => 1,
 			'fontsize'   => 100,
 			'gutter'     => 1,
-			'htmlscript' => 0,
 			'light'      => 0,
 			'ruler'      => 0,
 			'smarttabs'  => 1,
@@ -191,8 +190,8 @@ class SyntaxHighlighter {
 	}
 
 
-	// Output list of shortcode tags for the TinyMCE plugin
-	function output_javascript_shortcodes() {
+	// Output list of shortcode tags for the TinyMCE plugin as well as some CSS for the settings page
+	function admin_head() {
 		$tags = array();
 		$tags[] = 'sourcecode';
 		$tags[] = 'source';
@@ -204,6 +203,8 @@ class SyntaxHighlighter {
 		echo "<script type='text/javascript'>\n";
 		echo "	var syntaxHLcodes = '" . implode( '|', $tags ) . "';\n";
 		echo "</script>\n";
+
+		echo "<style type='text/css'>.bulletlist li { list-style-type: disc; margin-left: 25px; }</style>\n";
 	}
 
 
@@ -340,9 +341,6 @@ class SyntaxHighlighter {
 		if ( 1 != $this->settings['gutter'] )
 			echo "	SyntaxHighlighter.defaults['gutter'] = false;\n";
 
-		if ( 1 == $this->settings['htmlscript'] )
-			echo "	SyntaxHighlighter.defaults['html-script'] = true;\n";
-
 		if ( 1 == $this->settings['light'] )
 			echo "	SyntaxHighlighter.defaults['light'] = true;\n";
 
@@ -397,20 +395,21 @@ class SyntaxHighlighter {
 		// Default out all of the available parameters to "false" (easy way to check if they're set or not)
 		// Note this isn't the same as if the user passes the string "false" to the shortcode
 		$atts = apply_filters( 'syntaxhighlighter_shortcodeatts', shortcode_atts( array(
-			'language'    => false,
-			'lang'        => false,
-			'auto-links'  => false,
-			'class-name'  => false,
-			'collapse'    => false,
-			'first-line'  => false,
-			'gutter'      => false,
-			'highlight'   => false,
-			'html-script' => false,
-			'light'       => false,
-			'ruler'       => false,
-			'toolbar'     => false,
+			'language'   => false,
+			'lang'       => false,
+			'autolinks'  => false,
+			'classname'  => false,
+			'collapse'   => false,
+			'firstline'  => false,
+			'gutter'     => false,
+			'highlight'  => false,
+			'htmlscript' => false,
+			'light'      => false,
+			'ruler'      => false,
+			'smarttabs'  => false,
+			'tabsize'    => false,
+			'toolbar'    => false,
 		), $atts ) );
-
 
 		// Check for language shortcode tag such as [php]code[/php]
 		if ( isset($this->brushes[$tag]) ) {
@@ -454,13 +453,26 @@ class SyntaxHighlighter {
 		}
 
 		// Enable "htmlscript" for certain brushes
-		if ( false === $atts['html-script'] && in_array( $lang, apply_filters( 'syntaxhighlighter_htmlscriptbrushes', array( 'php' ) ) ) )
-			$atts['html-script'] = 'true';
+		//if ( false === $atts['html-script'] && in_array( $lang, apply_filters( 'syntaxhighlighter_htmlscriptbrushes', array( 'php' ) ) ) )
+		//	$atts['html-script'] = 'true';
 
 		foreach ( $atts as $key => $value ) {
 			if ( false === $value || in_array( $key, array( 'language', 'lang' ) ) )
 				continue;
 
+			// Parameter renaming (the shortcode API doesn't like parameter names with dashes)
+			$rename_map = array(
+				'autolinks'  => 'auto-links',
+				'classname'  => 'class-name',
+				'firstline'  => 'first-line',
+				'htmlscript' => 'html-script',
+				'smarttabs'  => 'smart-tabs',
+				'tabsize'    => 'tab-size',
+			);
+			if ( !empty($rename_map[$key]) )
+				$key = $rename_map[$key];
+
+			// If "html-script", then include the XML brush as it's needed
 			if ( $key == 'html-script' && ( 'true' == $value || 1 == $value ) )
 				$this->usedbrushes['xml'] = true;
 
@@ -540,8 +552,7 @@ class SyntaxHighlighter {
 					<label for="syntaxhighlighter-collapse"><input name="syntaxhighlighter-collapse" type="checkbox" id="syntaxhighlighter-collapse" value="1" <?php checked( $this->settings['collapse'], 1 ); ?> /> <?php _e('Collapse code boxes', 'syntaxhighlighter'); ?></label><br />
 					<label for="syntaxhighlighter-ruler"><input name="syntaxhighlighter-ruler" type="checkbox" id="syntaxhighlighter-ruler" value="1" <?php checked( $this->settings['ruler'], 1 ); ?> /> <?php _e('Show a ruler column along the top of the code box', 'syntaxhighlighter'); ?></label><br />
 					<label for="syntaxhighlighter-light"><input name="syntaxhighlighter-light" type="checkbox" id="syntaxhighlighter-light" value="1" <?php checked( $this->settings['light'], 1 ); ?> /> <?php _e('Use the light display mode, best for single lines of code', 'syntaxhighlighter'); ?></label><br />
-					<label for="syntaxhighlighter-smarttabs"><input name="syntaxhighlighter-smarttabs" type="checkbox" id="syntaxhighlighter-smarttabs" value="1" <?php checked( $this->settings['smarttabs'], 1 ); ?> /> <?php _e('Use smart tabs allowing tabs being used for alignment', 'syntaxhighlighter'); ?></label><br />
-					<label for="syntaxhighlighter-htmlscript"><input name="syntaxhighlighter-htmlscript" type="checkbox" id="syntaxhighlighter-htmlscript" value="1" <?php checked( $this->settings['htmlscript'], 1 ); ?> /> <?php _e('If mixing HTML/XML code with other code, highlight both languages', 'syntaxhighlighter'); ?></label><br />
+					<label for="syntaxhighlighter-smarttabs"><input name="syntaxhighlighter-smarttabs" type="checkbox" id="syntaxhighlighter-smarttabs" value="1" <?php checked( $this->settings['smarttabs'], 1 ); ?> /> <?php _e('Use smart tabs allowing tabs being used for alignment', 'syntaxhighlighter'); ?></label>
 				</fieldset>
 			</td>
 		</tr>
@@ -553,8 +564,11 @@ class SyntaxHighlighter {
 			<th scope="row"><label for="syntaxhighlighter-firstline"><?php _e('Starting Line Number', 'syntaxhighlighter'); ?></label></th>
 			<td><input name="syntaxhighlighter-firstline" type="text" id="syntaxhighlighter-firstline" value="<?php echo attribute_escape( $this->settings['firstline'] ); ?>" class="small-text" /></td>
 		</tr>
-		<tr valign="top">
-			<th scope="row"><label for="syntaxhighlighter-fontsize"><?php _e('Font Size (Percentage)', 'syntaxhighlighter'); ?></label></th>
+		<tr valign="top" style="display:none">
+			<th scope="row">
+				<label for="syntaxhighlighter-fontsize"><?php _e('Font Size (Percentage)', 'syntaxhighlighter'); ?></label>
+				<br /><strong>THIS CURRENTLY DOESN'T WORK</strong>
+			</th>
 			<td><input name="syntaxhighlighter-fontsize" type="text" id="syntaxhighlighter-fontsize" value="<?php echo attribute_escape( $this->settings['fontsize'] ); ?>" class="small-text" />%</td>
 		</tr>
 		<tr valign="top">
@@ -574,7 +588,65 @@ class SyntaxHighlighter {
 
 	<p><?php _e('Click &quot;Save Changes&quot; to update this preview.', 'syntaxhighlighter'); ?>
 
-	<?php global $post; $post->ID = 0; echo $this->parse_shortcodes( "[php]<?php echo 'Hello world!'; ?>[/php]" ); ?>
+	<?php
+
+		global $post;
+
+		$post->ID = 0;
+
+		$democode = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>PHP Code Example</title>
+</head>
+<body>
+	<h1>PHP Code Example</h1>
+
+	<p><?php echo \'Hello World!\'; ?></p>
+
+	<p>This line is highlighted.</p>
+
+	<div class="foobar">
+		This	is	an
+		example	of	smart
+		tabs.
+	</div>
+
+	<p><a href="http://wordpress.org/">WordPress</a></p>
+</body>
+</html>';
+
+		echo $this->parse_shortcodes( '[php htmlscript="true" highlight="12"]' . $democode . '[/php]' );
+
+?>
+
+	<h3 style="margin-top:30px"><?php _e('Shortcode Parameters', 'syntaxhighlighter'); ?></h3>
+
+	<p><?php printf( __('These are the parameters you can pass to the shortcode and what they do. For the booleans (i.e. on/off), pass %1$s/%2$s or %3$s/%4$s.', 'syntaxhighlighter'), '<code>true</code>', '<code>1</code>', '<code>false</code>', '<code>0</code>' ); ?></p>
+
+	<ul class="bulletlist">
+		<li><?php printf( __('%1$s or %2$s &#8212; The language syntax to highlight with. You can alternately just use that as the tag, such as <code>[php]code[/php]</code>. <a href="%3$s">Click here</a> for a list of valid tags (under &quot;aliases&quot;).', 'syntaxhighlighter'), '<code>lang</code>', '<code>language</code>', 'http://alexgorbatchev.com/wiki/SyntaxHighlighter:Brushes' ); ?></li>
+		<li><?php printf( __('%s &#8212; Toggle automatic URL linking.', 'syntaxhighlighter'), '<code>autolinks</code>' ); ?></li>
+		<li><?php printf( __('%s &#8212; Add an additional CSS class to the code box.', 'syntaxhighlighter'), '<code>classname</code>' ); ?></li>
+		<li><?php printf( __('%s &#8212; Toggle collapsing the code box by default, requiring a click to expand it. Good for large code posts.', 'syntaxhighlighter'), '<code>collapse</code>' ); ?></li>
+		<li><?php printf( __('%s &#8212; An interger specifying what number the first line should be (for the line numbering).', 'syntaxhighlighter'), '<code>firstline</code>' ); ?></li>
+		<li><?php printf( __('%s &#8212; Toggle the left-side line numbering.', 'syntaxhighlighter'), '<code>gutter</code>' ); ?></li>
+		<li><?php printf( __('%s &#8212; A comma-sperated list of line numbers to highlight.', 'syntaxhighlighter'), '<code>highlight</code>' ); ?></li>
+		<li><?php printf( __("%s &#8212; Toggle highlighting any extra HTML/XML. Good for when you're mixing HTML/XML with another language, such as having PHP inside an HTML web page.", 'syntaxhighlighter'), '<code>htmlscript</code>' ); ?></li>
+		<li><?php printf( __("%s &#8212; Toggle light mode which disables the gutter, toolbar, and ruler all at once.", 'syntaxhighlighter'), '<code>light</code>' ); ?></li>
+		<li><?php printf( __("%s &#8212; Toggle the column ruler at the top of the code box.", 'syntaxhighlighter'), '<code>ruler</code>' ); ?></li>
+		<li><?php printf( __("%s &#8212; Toggle the toolbar containing the helpful buttons.", 'syntaxhighlighter'), '<code>toolbar</code>' ); ?></li>
+	</ul>
+
+	<p><?php _e('Some example shortcodes:', 'syntaxhighlighter'); ?></p>
+
+	<ul class="bulletlist">
+		<li><code>[php]<?php _e('your code here', 'syntaxhighlighter'); ?>[/php]</code></li>
+		<li><code>[css autolinks=&quot;false&quot; classname=&quot;myclass&quot; collapse=&quot;false&quot; firstline=&quot;1&quot; gutter=&quot;true&quot; highlight=&quot;1,2,3&quot; htmlscript=&quot;false&quot; light=&quot;false&quot; ruler=&quot;false&quot; smarttabs=&quot;true&quot; tabsize=&quot;4&quot; toolbar=&quot;true&quot;]<?php _e('your code here', 'syntaxhighlighter'); ?>[/css]</code></li>
+		<li><code>[code lang=&quot;js&quot;]<?php _e('your code here', 'syntaxhighlighter'); ?>[/code]</code></li>
+		<li><code>[sourcecode language=&quot;plain&quot;]<?php _e('your code here', 'syntaxhighlighter'); ?>[/sourcecode]</code></li>
+	</ul>
 
 <?php $this->maybe_output_scripts(); ?>
 
@@ -606,7 +678,6 @@ class SyntaxHighlighter {
 			$settings['autolinks']  = ( !empty($_POST['syntaxhighlighter-autolinks']) )  ? 1 : 0;
 			$settings['collapse']   = ( !empty($_POST['syntaxhighlighter-collapse']) )   ? 1 : 0;
 			$settings['gutter']     = ( !empty($_POST['syntaxhighlighter-gutter']) )     ? 1 : 0;
-			$settings['htmlscript'] = ( !empty($_POST['syntaxhighlighter-htmlscript']) ) ? 1 : 0;
 			$settings['light']      = ( !empty($_POST['syntaxhighlighter-light']) )      ? 1 : 0;
 			$settings['ruler']      = ( !empty($_POST['syntaxhighlighter-ruler']) )      ? 1 : 0;
 			$settings['smarttabs']  = ( !empty($_POST['syntaxhighlighter-smarttabs']) )  ? 1 : 0;
